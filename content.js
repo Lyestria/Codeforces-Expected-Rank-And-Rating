b931015cd87ac7ef03eb8f28b25a1da38072a427
Kevin Wan <kevin.wan59049@gmail.com>
2020-09-13 03:34:13 -0400

    Basic fetch and evaluate functions acheived (may be buggy)

--- content.js
+++ content.js
@@ -1,72 +1,42 @@
 async function getJSON(url) {
-	let res = await fetch(url);
-	return await res.json();
+	return (await fetch(url)).json();
 }
 
-function assign_ranks(ranks, len) {
-	var rk = (ranks.length + len-1) * 0.5 + 1;
-	while (ranks.length < len) ranks.push(rk);
+async function getRating(user){
+	return (await getJSON(`https://codeforces.com/api/user.info?handles=${user}`)).result[0].rating;
 }
 
-var times = {};
-async function get_time(code){
-	if (times[code] !== undefined) return times[code];
-	obj = await getJSON("https://dmoj.ca/api/contest/info/" + code);
-	return times[code] = new Date(obj.end_time);
+function assign_ranks(contestents, s, e) {
+	let rk = (s+e-1)*0.5;
+	while (s< len) contestents[s++]=rk;
 }
-async function do_stuff() {
-	var path = window.location.pathname;
-	var segs = path.split('/');
-	var code = segs[segs.length - 3];
+
+async function main() {
+	var code = window.location.pathname.split('/')[2];
 	console.log(code);
 
-	var obj = await getJSON("https://dmoj.ca/api/contest/info/" + code)
-	var ranking = obj.rankings;
-	var n = ranking.length;
-	var old_rating = Array(), old_volatility = Array(), ranks = Array(), times_rated = Array(), new_ind = Array(n);
-	var curTime = new Date(obj.end_time);
+	var obj = getJSON(`https://codeforces.com/api/contest.standings?contestId=${code}`);
+	if(obj.status != "OK")return;
 
-	var lastEqual = -1;
+	var ranking = Object.entries(obj.result.rows).map((e) => ({handle: e[1].party.members[0].handle, rank: e[1].rank}));
+	ranking.forEach((e) => e[1].rating = await getRating(e[0]));
 
-	for (var i = 0, ind = 0; i < n; i++) {
-		var num = 0;
-		ranking[i].solutions.forEach(sol => num += sol !== null);
-		if (num == 0) continue;
-		new_ind[i] = ind;
-
-		if (lastEqual == -1 || ranking[i].points != ranking[lastEqual].points || ranking[i].cumtime != ranking[lastEqual].cumtime) {
-			assign_ranks(ranks, old_rating.length);
-			lastEqual = i;
+	let beg=0;
+	for (let i=1; i<n; i++){
+		if(ranking[i].rank != ranking[i-1].rank){
+			assign_ranks(ranking,beg,i);
+			beg=i;
 		}
-		old_rating.push(0), old_volatility.push(0), times_rated.push(0);
-
-		var username = ranking[i].user;
-		var contests = (await getJSON("https://dmoj.ca/api/user/info/" + username)).contests;
-		old_rating[ind] = 1200;
-		old_volatility[ind] = 535;
-		times_rated[ind] = 0;
-		recentTime = new Date(-100000000000000);
-		for (var contest in contests.history) {
-			if (contests.history[contest].rating === null) continue;
-			var time = await get_time(contest);
-			if (time >= curTime) continue;
-			times_rated[ind]++;
-			if (time > recentTime) {
-				old_rating[ind] = contests.history[contest].rating;
-				old_volatility[ind] = contests.history[contest].volatility;
-			}
-		}
-		ind++;
 	}
-	assign_ranks(ranks, old_rating.length);
+	assign_ranks(ranking,beg,n);
 
-	var new_rating = recalculate_ratings(old_rating, old_volatility, ranks, times_rated);
+	calculateExpectedRank(ranking);
+	calculateDeltas(ranking);
 
-	var deltas = Array(n);
-	for (var i = 0; i < n; i++)deltas[i] = new_ind[i] === undefined ? 0 : new_rating[new_ind[i]] - old_rating[new_ind[i]];
-
-	console.log(deltas);
-
+	ranking = ranking.reduce((acc,cur) => {
+		acc[cur.handle]=cur;
+	}, {})
+	/*
 	$('#users-table').children().eq(0).children().eq(0).append("<th class=\"rank\">+</th>");
 	var rows = $('#users-table').children().eq(1).children();
 	for (var i = 0; i < n; i++) {
@@ -81,5 +51,6 @@ async function do_stuff() {
 	console.log(old_volatility);
 	console.log(times_rated);
 	console.log(deltas.reduce((a, c) => a + c));
+	*/
 }
-do_stuff();
+main();
